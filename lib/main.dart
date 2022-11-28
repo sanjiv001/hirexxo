d81c5b63216ea8b56edbf0536a870762f9ff671c
Sujal <you@gmail.com>
2022-11-28 12:15:02 +0545

    4th commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:hirexxo/screen/botton_navigation_bar.dart';
 import 'package:hirexxo/screen/welcome_screen.dart';
 
 Future<void> main() async {
@@ -21,7 +22,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const WelcomeScreen(),
+      home: RootApp(),
     );
   }
 }
